8ddbe336f2b299145560af03fc4bc8e4d8359797
z3b0w <andryslovas@gmail.com>
2021-12-17 15:17:18 +0200

    Added server + users + dbHELPER

---
+++ src/routes/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+//GET all users
+router.get('/users', getUsers);
+
+//POST create user
+router.post('/user', createUser);
+
+//GET single user
+// router.get('/user/:id', getUsers);
+
+//DELETE user
+router.delete('/users/:id', deleteUser);
+
+//PUT update user
+router.put('/users/:id', updateUser);
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -5,9 +5,19 @@ const cors = require('cors');
 const app = express();
 
 const { PORT } = require('./config');
+const { dbSuccess } = require('./utils/dbHelper');
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  const sql = `INSERT INTO users (name, age, email, password) VALUES ('jane', 20, jane@doe.com, '123456')`;
+
+  db.query(sql, (err, result) => {
+    res.send('ABC');
+  });
+});
+
+app.post('/user', (req, res) => {
+  const sql = `INSERT INTO users (name, age, email, password) VALUES (?, ?, ?, ?)`;
+  db.query(sql, []);
 });
 
 app.listen(PORT, () => {
