f1689948accab31c6911342c3fa79022fb3263f8
z3b0w <andryslovas@gmail.com>
2021-12-19 13:43:40 +0200

    added routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,19 @@
 import express from 'express';
 
 import {
-  getAllProducts,
-  
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
 } from '../controllers/Users.js';
 
 const router = express.Router();
 
-router.get('/', getAllProducts);
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
